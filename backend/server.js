1ec570f4b5ed19bcd3ff535283161b1a3388c89f
Darcy <mr_darcy_1@mail.ru>
2025-06-16 12:11:09 +0400

    [Бэкенд] Реализация API для авторизации и получения данных пользователя (#3)
    * feat: implement auth APIs (login, signup, get user)
    
    * feat: implement auth APIs (login, signup, get user) - part 2

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db")
 
+const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
+
 const app = express();
 
 // Middleware to handle CORS
@@ -24,8 +27,8 @@ app.use(express.json());
 
 // Routes
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
 
