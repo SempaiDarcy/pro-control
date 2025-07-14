004d60768135e1d88a66aefff565c268e7f9483a
SempaiDarcy <mr_darcy_1@mail.ru>
2025-07-14 11:05:46 +0500

    feat: manage tasks

--- frontend/Pro-Control/src/components/avatar-group.jsx
+++ frontend/Pro-Control/src/components/avatar-group.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-export const AvatarGroup = ({avatars, maxVisible = 3}) => {
+export const AvatarGroup = ({ avatars, maxVisible = 3 }) => {
     return (
         <div className="flex items-center">
-            {avatars.slice(0, maxVisible).map((avatar, index) => (
-                <img
-                    key={index}
-                    src={avatar}
-                    alt={`Avatar ${index}`}
-                    className="w-9 h-9 rounded-full border-2 border-white -ml-3 first:ml-0"
-                />
-            ))}
-            {avatars.length > maxVisible && (
+            {avatars
+                .filter((avatar) => avatar) // убираем null, undefined, ""
+                .slice(0, maxVisible)
+                .map((avatar, index) => (
+                    <img
+                        key={index}
+                        src={avatar}
+                        alt={`Avatar ${index}`}
+                        className="w-9 h-9 rounded-full border-2 border-white -ml-3 first:ml-0"
+                    />
+                ))}
+            {avatars.filter((avatar) => avatar).length > maxVisible && (
                 <div className="w-9 h-9 flex items-center justify-center bg-blue-50 text-sm font-medium rounded-full border-2 border-white -ml-3">
                     +{avatars.length - maxVisible}
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
--- frontend/Pro-Control/src/pages/admin/manage-tasks.jsx
+++ frontend/Pro-Control/src/pages/admin/manage-tasks.jsx
@@ -1,9 +1,112 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { DashboardLayout } from "../../components/layouts/dashboard-layout.jsx";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axios-instance.js";
+import { API_PATHS } from "../../utils/api-paths.js";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import { TaskStatusTabs } from "../../components/task-status-tabs.jsx";
+import { TaskCard } from "../../components/Cards/task-card.jsx";
+
+// Маппинг для фильтрации и отображения
+const statusMap = {
+    "Все": "",
+    "Ожидает": "Pending",
+    "В работе": "In Progress",
+    "Завершено": "Completed",
+};
 
 export const ManageTasks = () => {
+    const [allTasks, setAllTasks] = useState([]);
+    const [tabs, setTabs] = useState([]);
+    const [filterStatus, setFilterStatus] = useState("Все");
+    const navigate = useNavigate();
+
+    const getAllTasks = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+                params: {
+                    status: statusMap[filterStatus] || "",
+                },
+            });
+
+            setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+            const statusSummary = response.data?.statusSummary || {};
+
+            const statusArray = [
+                { label: "Все", count: statusSummary.all || 0 },
+                { label: "Ожидает", count: statusSummary.pendingTasks || 0 },
+                { label: "В работе", count: statusSummary.inProgressTasks || 0 },
+                { label: "Завершено", count: statusSummary.completedTasks || 0 },
+            ];
+
+            setTabs(statusArray);
+        } catch (error) {
+            console.error("Ошибка при получении задач:", error);
+        }
+    };
+
+    const handleClick = (taskData) => {
+        navigate(`/admin/create-task`, { state: { taskId: taskData._id } });
+    };
+
+    useEffect(() => {
+        getAllTasks(filterStatus);
+    }, [filterStatus]);
+
     return (
-        <div>
-            ManageTasks
-        </div>
+        <DashboardLayout activeMenu="Manage Tasks">
+            <div className="my-5">
+                <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+                    <div className="flex items-center justify-between gap-3">
+                        <h2 className="text-xl md:text-xl font-medium">Мои задачи</h2>
+
+                        <button
+                            className="flex lg:hidden download-btn"
+                        >
+                            <LuFileSpreadsheet className="text-lg" />
+                            Скачать отчет
+                        </button>
+                    </div>
+
+                    {tabs?.[0]?.count > 0 && (
+                        <div className="flex items-center gap-3">
+                            <TaskStatusTabs
+                                tabs={tabs}
+                                activeTab={filterStatus}
+                                setActiveTab={setFilterStatus}
+                            />
+
+                            <button
+                                className="hidden lg:flex download-btn"
+                            >
+                                <LuFileSpreadsheet className="text-lg" />
+                                Скачать отчет
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+                    {allTasks?.map((item) => (
+                        <TaskCard
+                            key={item._id}
+                            title={item.title}
+                            description={item.description}
+                            priority={item.priority}
+                            status={item.status}
+                            progress={item.progress}
+                            createdAt={item.createdAt}
+                            dueDate={item.dueDate}
+                            assignedTo={item.assignedTo?.map((user) => user.profileImageUrl)}
+                            attachmentCount={item.attachments?.length || 0}
+                            completedTodoCount={item.completedTodoCount || 0}
+                            todoChecklist={item.todoChecklist || []}
+                            onClick={() => handleClick(item)}
+                        />
+                    ))}
+                </div>
+            </div>
+        </DashboardLayout>
     );
 };
